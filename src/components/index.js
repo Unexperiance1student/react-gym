584ab68f2e740e73ecde43268e44c3ffa0d274b9
Vladislav <75188379+Unexperiance1student@users.noreply.github.com>
2023-01-16 00:03:10 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import Exercises from './Exercises';
 import Footer from './Footer';
+import HeroBanner from './HeroBanner';
 import Navbar from './Navbar';
-export { Navbar, Footer };
+import SearchExercises from './SearchExercises';
+export { Navbar, Footer, HeroBanner, SearchExercises, Exercises };
