3752f04be45da5c53685f77f8754d6772a9ab597
Vladislav <75188379+Unexperiance1student@users.noreply.github.com>
2023-01-16 03:55:42 +0300

    Create BodyPart.jsx

---
+++ src/components/BodyPart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
+  return <div>BodyPart</div>;
+};
+
+export default BodyPart;
